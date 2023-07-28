c82027703143acb99e8ecb3091f7c8f1e01c06d2
satabdi <deysatabdi70@gmail.com>
2023-07-28 12:00:51 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 def data_render(request):
     d = {'name':'Satabdi','age':24}
     return render(request,'data_render.html',context=d)
+
+def if_else(request):
+    d = {'a':10, 'b' : 5}
+    return render(request,'if_else.html',context=d)
