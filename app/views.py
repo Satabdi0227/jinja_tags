ccee7a792d980d1b3b4a003cb710652e9ca3955c
satabdi <deysatabdi70@gmail.com>
2023-07-28 12:10:55 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -8,3 +8,7 @@ def data_render(request):
 def if_else(request):
     d = {'a':10, 'b' : 5}
     return render(request,'if_else.html',context=d)
+
+def if_elif_else(request):
+    d = {'a' : 10, 'b' : 5,'c': 20 }
+    return render(request,'if_elif_else.html',context=d)
